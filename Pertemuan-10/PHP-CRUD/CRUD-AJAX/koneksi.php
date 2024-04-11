deff1f2abefac1f25d182a396944f77e125e4558
Agung-Nugroho <117786580+Zippy-Ux@users.noreply.github.com>
2024-04-11 14:07:54 +0700

    First commit

---
+++ Pertemuan-10/PHP-CRUD/CRUD-AJAX/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB1','prakwedb');
+$db1 = new mysqli(HOST, USER, PASS, DB1);
+
+if ($db1->connect_error) {
+    die("Koneksi gagal: " . $db1->connect_error);
+}
