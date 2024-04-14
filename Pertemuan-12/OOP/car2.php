e54b356153d246826765605407323fdd108c48d8
Agung-Nugroho <117786580+Zippy-Ux@users.noreply.github.com>
2024-04-14 19:42:00 +0700

    First commit

---
+++ Pertemuan-12/OOP/car2.php
@@ -0,0 +1,27 @@
+<?php
+class Car2 {
+    private $brand;
+
+    public function __construct($brand)
+    {
+        echo "A new car is created. <br>";
+        $this->brand = $brand;
+    }
+
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function __destruct()
+    {
+        echo "The car is destroyed.<br>";
+    }
+}
+
+$car = new Car2("Toyota");
+
+echo "Brand: " . $car->getBrand() . "<br>";
+
+
+
+?>
